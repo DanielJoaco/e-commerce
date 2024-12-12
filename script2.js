94e72a739c3231bad81408c6eba9ac68d4a477c1
Daniel Joaco <danieljorjuela@gmail.com>
2024-12-11 19:48:15 -0500

    Creacion de proyecto con react

--- script2.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const enviarButton = document.getElementById('enviar');
-    enviarButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        window.location.href = 'index.html';
-        console.log('Botón enviar presionado');
-    });
-});
